fe423a6314cff18152879ad66f25a775735817b9
Geraint White <geraintwhite@gmail.com>
2015-09-04 23:15:16 +0100

    Three arcs draw correctly

--- src/UsageMonitor.c
+++ src/UsageMonitor.c
@@ -8,31 +8,16 @@ enum {
   HEADER_SIZE = 16
 };
 
-struct Arc {
+typedef struct {
   float percent;
   int radius;
-  Layer layer;
-  void update_proc;
-};
+} Arc;
 
 static Window *window;
 static TextLayer *text_layer;
-static Layer *arc_layer;
-
-struct Arc arc1;
-arc1.percent = 75;
-arc1.radius = 60;
-arc1.update_proc = arc1_update_proc;
-
-struct Arc arc2;
-arc2.percent = 50;
-arc2.radius = 40;
-arc2.update_proc = arc2_update_proc;
-
-struct Arc arc3;
-arc3.percent = 25;
-arc3.radius = 20;
-arc3.update_proc = arc3_update_proc;
+static Layer *arc1_layer;
+static Layer *arc2_layer;
+static Layer *arc3_layer;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -52,36 +37,43 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-static void create_arc_layer(struct arc) {
-  arc.layer = layer_create(bounds);
-  layer_set_update_proc(arc.layer, arc.update_proc);
-  layer_add_child(window_layer, arc.layer);
-}
+static void arc_update_proc(Layer *layer, GContext *ctx) {
+  Arc *arc = (Arc*) layer_get_data(layer);
 
-static void draw_arc(GContext *ctx, struct arc) {
   GPoint origin = GPoint(SCREEN_WIDTH / 2, (SCREEN_HEIGHT - HEADER_SIZE) / 2);
-  graphics_draw_arc(ctx, origin, arc.radius, CIRCLE_THICKNESS, 270, 270 + 360 * arc.percent);
+  graphics_draw_arc(ctx, origin, arc->radius, CIRCLE_THICKNESS, 0, 360 * arc->percent);
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Origin (%d, %d)", SCREEN_WIDTH / 2, (SCREEN_HEIGHT - HEADER_SIZE) / 2);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Radius %d, Percent %d", arc->radius, (int) (arc->percent * 100));
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Start 270, End %d", (int) (360 * arc->percent));
 }
 
-static void arc1_update_proc(Layer *layer, GContext *ctx) {
-  draw_arc(ctx, arc1);
-}
+static void create_arc_layer(Layer *window_layer, GRect bounds, Layer *arc_layer, Arc *arc) {
+  arc_layer = layer_create_with_data(bounds, sizeof(Arc));
+  layer_set_update_proc(arc_layer, arc_update_proc);
+  layer_add_child(window_layer, arc_layer);
 
-static void arc2_update_proc(Layer *layer, GContext *ctx) {
-  draw_arc(ctx, arc2);
-}
-
-static void arc3_update_proc(Layer *layer, GContext *ctx) {
-  draw_arc(ctx, arc3);
+  Arc *arc_ctx = (Arc*) layer_get_data(arc_layer);
+  arc_ctx->radius = arc->radius;
+  arc_ctx->percent = arc->percent;
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  create_arc_layer(arc1);
-  create_arc_layer(arc2);
-  create_arc_layer(arc3);
+  create_arc_layer(window_layer, bounds, arc1_layer, &(Arc) {
+    .percent = 0.75,
+    .radius = 60
+  });
+  create_arc_layer(window_layer, bounds, arc2_layer, &(Arc) {
+    .percent = 0.5,
+    .radius = 40
+  });
+  create_arc_layer(window_layer, bounds, arc3_layer, &(Arc) {
+    .percent = 0.25,
+    .radius = 20
+  });
 
   // text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   // text_layer_set_text(text_layer, "Press a button");
@@ -91,7 +83,9 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
-  layer_destroy(arc_layer);
+  layer_destroy(arc1_layer);
+  layer_destroy(arc2_layer);
+  layer_destroy(arc3_layer);
 }
 
 static void init(void) {
